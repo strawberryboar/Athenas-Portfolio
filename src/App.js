eefee03240ff6faf60d78473ed8bc1e8e419e2bc
strawberryboar <athenal.petrovich@gmail.com>
2023-04-03 18:27:40 -0600

    Laying foundation for app
    - Added placeholder text and asset
    - Removed default react code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import catPng from './assets/cat.png';
 
 function App() {
   return (
     <div className="App">
+      <div className='appWrapper'>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          Portfolio
+        </h1>
       </header>
+    </div>
+    <div className='intro'>
+      <img src={catPng} />
+      <h2>
+        Mow
+      </h2>
+    </div>
     </div>
   );
 }
