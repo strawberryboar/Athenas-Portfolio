b356e285d2537f3c854a4d2669aec467c5b51940
strawberryboar <athenal.petrovich@gmail.com>
2023-04-09 00:12:51 -0600

    Removed apollo/graphql, added buttons to project imgs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,4 @@
 import './App.css';
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
-} from "@apollo/client";
-import { setContext } from "@apollo/client/link/context";
 import Homepage from './pages/Homepage.js';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Footer from './components/Footer.js';
@@ -14,38 +7,15 @@ import Contact from './pages/Contact.js';
 import Socials from './pages/Socials.js';
 
 function App() {
-  
-  const httpLink = createHttpLink({
-    uri: "/graphql"
-  });
-
-  const authLink = setContext((_, {headers }) => {
-    const token = localStorage.getItem("id_token");
-
-    return {
-      headers: {
-        ...headers,
-        authorization: token ? 'Bearer ${token}' : "",
-      },
-    };
-  });
-
-  const client = new ApolloClient({
-    link: authLink.concat(httpLink),
-    cache: new InMemoryCache(),
-  });
-
   return (
     <>
     <Router>
-      <ApolloProvider client={client}>
       <Routes>
         <Route path='/' element={<Homepage/>}/>
         <Route path='/contact' element={<Contact/>}/>
         <Route path='/socials' element={<Socials/>}/>
       </Routes>
       <Footer />
-      </ApolloProvider>
     </Router>
     </>
   );
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import catPng from '../assets/cat.png';
 import catPng2 from '../assets/cat2.png';
 import catPng3 from '../assets/cat3.png';
@@ -20,6 +20,8 @@ export default function Homepage() {
     function handleClick() {
         learnRef.current.scrollIntoView({ behavior: "smooth" });
       }
+
+    const [isShown, setIsShown] = useState(false);
 
     return(
     <div className="Homepage">
@@ -64,9 +66,56 @@ export default function Homepage() {
                 </div>
             </div>
         </div>
+        <div className='projects'>
+            <h1>My Work|</h1>
+            <div className='projBoxContain'>
+            <div className='projBox'>
+                <h2>HAUL'R</h2>
+                <button
+                onMouseEnter={() => setIsShown(true)}
+                onMouseLeave={() => setIsShown(false)}>
+                <img src={catPng}></img>
+                </button>
+                {isShown && (
+                    <div>
+                        appears when you hover over the button
+                    </div>
+                )}
+                <p>MERN stack site that's hosted on an AWS EC2 instance running Ubuntu and Apache, sharing the same servers as the HAUL'R iOS application</p>
+            </div>
+            <div className='projBox'>
+                <h2>Meal Assistant</h2>
+                <button
+                onMouseEnter={() => setIsShown(true)}
+                onMouseLeave={() => setIsShown(false)}>
+                <img src={catPng}></img>
+                </button>
+                {isShown && (
+                    <div>
+                        appears when you hover over the button
+                    </div>
+                )}
+                <p>The Meal Assistant is an app that allows users to search for both food and drink recipes by name/title, search for recipes by ingredient, browse various recipes via a catalog, and save various meal recipes and cocktail drink recipes. This application can even generate random recipes if they are not sure what they want.</p>
+            </div>
+            <div className='projBox'>
+                <h2>website</h2>
+                <button
+                onMouseEnter={() => setIsShown(true)}
+                onMouseLeave={() => setIsShown(false)}>
+                <img src={catPng}></img>
+                </button>
+                {isShown && (
+                    <div>
+                        appears when you hover over the button
+                    </div>
+                )}
+                <p>peepee</p>
+            </div>
+            </div>
+        </div>
         <div className='divBox' id='divBox2'>
-            <h1>What I Offer</h1>
-            <p>pricing and stuff</p>
+            <h1>Languages I Use</h1>
+            <p>Content</p>
         </div>
         <div className='divBox' id='divBox3'>
             <h1>Socials</h1>
@@ -85,26 +134,7 @@ export default function Homepage() {
                 </div>
             </div>
         </div>
-        <div className='projects'>
-            <h1>My Work|</h1>
-            <div className='projBoxContain'>
-            <div className='projBox'>
-                <h2>HAUL'R</h2>
-                <img src={catPng}></img>
-                <p>MERN stack site that's hosted on an AWS EC2 instance running Ubuntu and Apache, sharing the same servers as the HAUL'R iOS application</p>
-            </div>
-            <div className='projBox'>
-                <h2>Meal Assistant</h2>
-                <img src={catPng}></img>
-                <p>The Meal Assistant is an app that allows users to search for both food and drink recipes by name/title, search for recipes by ingredient, browse various recipes via a catalog, and save various meal recipes and cocktail drink recipes. This application can even generate random recipes if they are not sure what they want.</p>
-            </div>
-            <div className='projBox'>
-                <h2>website</h2>
-                <img src={catPng}></img>
-                <p>peepee</p>
-            </div>
-            </div>
-        </div>
+        
     </div>
     )
 }
