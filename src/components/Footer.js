2e7cd5bb358277d19b4f3f9d947f5d1909545c0d
Athena Petrovich <51300273+strawberryboar@users.noreply.github.com>
2023-04-15 14:38:33 -0600

    Replaced footer svgs, styled some buttons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,27 +1,25 @@
 import React from 'react';
 import './Footer.css';
-import { Link } from 'react-router-dom';
-import Email from '../assets/email.svg';
-import Linkedin from '../assets/linkedin.svg';
-import Github from '../assets/github.svg';
-import Twitter from '../assets/twitter.svg';
-import {BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { ReactComponent as Linkedin} from '../assets/Linkedin1.svg';
+import { ReactComponent as Github} from '../assets/Github1.svg';
+import { ReactComponent as Twitter} from '../assets/Twitter1.svg';
+import { ReactComponent as Email} from '../assets/Email1.svg';
 
 export default function Footer() {
     return(
         <footer>
         <div className='footerWrap'>
             <div className='footerBox'>
-                <a href='mailto:a.petrovich.dev@gmail.com' target='_blank'><svg src={Email}></svg></a>
+                <a href='mailto:a.petrovich.dev@gmail.com' target='_blank'><Email width='100px' height='100px' /></a>
                 </div>
                 <div className='footerBox'>
-                <a href='https://github.com/strawberryboar' target='_blank'><svg src={Github}></svg></a>
+                <a href='https://github.com/strawberryboar' target='_blank'><Github width='100px' height='100px' /></a>
                 </div>
                 <div className='footerBox'>
-                <a href='https://twitter.com/APetrovichDev' target="_blank"><svg src={Twitter}></svg></a>
+                <a href='https://twitter.com/APetrovichDev' target="_blank"><Twitter width='100px' height='100px' /></a>
                 </div>
                 <div className='footerBox'>
-                <a href='https://www.linkedin.com/in/athena-petrovich-b238321a7/' target='_blank'><svg src={Linkedin}></svg></a>
+                <a href='https://www.linkedin.com/in/athena-petrovich-b238321a7/' target='_blank'><Linkedin width='100px' height='100px' /></a>
                 </div>
         </div>
         <p>&copy;peepee</p>
