3ff59ac5487d9a705ed6b6e06d5d52a3a7bc6dea
strawberryboar <athenal.petrovich@gmail.com>
2023-05-10 15:25:25 -0600

    updated linkbutton image

--- src/components/LinkButton.js
+++ src/components/LinkButton.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import './LinkButton.css';
 import catPng from '../assets/cat.png';
-import HaulrGif from '../assets/HAULR-2.0.gif';
+import HaulrScreen from '../assets/haulrscreen.png';
 
 export default function LinkButton() {
 
@@ -10,7 +10,7 @@ export default function LinkButton() {
 
     return(
         <div className='btnDiv'>
-        <img src={HaulrGif}
+        <img src={HaulrScreen}
             onMouseEnter={() => {
                 setImgOpacity(0.5)
                 setIsShown(true)
